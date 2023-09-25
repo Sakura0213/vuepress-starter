369020caf90b71cf048268bf77abcd519726b46b
Sakura0213 <zengxin@email.cn>
2023-09-25 23:55:19 +0800

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,8 @@
 call npm run build
-
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:Sakura0213/vuepress-starter.git master &
-
+call git init
+call git add -A
+call git commit -m 'deploy'
+call git push -f git@github.com:Sakura0213/vuepress-starter.git master
 cd docs/.vuepress/dist
 git init
 git add -A
