4c80cbd0416cc5848a92c31b3cfce1d6f2056dc3
Sakura0213 <zengxin@email.cn>
2023-09-27 13:54:16 +0800

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -3,8 +3,4 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:Sakura0213/vuepress-starter.git master
-cd docs/.vuepress/dist
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:Sakura0213/Sakura0213.github.io.git master
+
